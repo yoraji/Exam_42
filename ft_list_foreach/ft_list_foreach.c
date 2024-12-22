b0c02ea67783d60bbdc88db375f9a913d08a372f
yoraji <rajiy8299@gmail.com>
2024-12-22 19:01:17 +0100

    updated

--- ft_list_foreach/ft_list_foreach.c
+++ ft_list_foreach/ft_list_foreach.c
@@ -1,11 +1,62 @@
-
 #include "ft_list.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+t_list *ft_lstnew(int num)
 {
-	while (begin_list)
-	{
-		(*f)(begin_list->data);
-		begin_list = begin_list->next;
-	}
+    t_list *head = malloc(sizeof(t_list));
+    if (!head)
+        return NULL;
+    head->data = num;
+    head->next = NULL;
+    return head;
 }
+
+void ft_list_print(t_list *lst)
+{
+    while (lst)
+    {
+        printf("%d -> ", lst->data);
+        lst = lst->next;
+    }
+    printf("NULL\n");
+}
+
+void ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    while (begin_list)
+    {
+        (*f)(&(begin_list->data));
+        begin_list = begin_list->next;
+    }
+}
+
+void increment_value(void *data)
+{
+    int *num = (int *)data;
+    *num += 10;
+}
+
+void print_value(void *data)
+{
+    int *num = (int *)data;
+    printf("%d ", *num);
+}
+
+/*
+ * ---------------------------| if you would like to test it fuction |------------- 
+int main()
+	{
+		t_list *node = ft_lstnew(290);
+		node->next = ft_lstnew(380);
+		node->next->next = ft_lstnew(200);
+		node->next->next->next = ft_lstnew(125);
+		ft_list_print(node);
+		ft_list_foreach(node, increment_value);
+		ft_list_print(node);
+		ft_list_foreach(node, print_value);
+		printf("\n");
+		return 0;
+	}
+-----------------------------------------------------------------------------------
+*/
