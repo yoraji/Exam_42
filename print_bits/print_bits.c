629aeb8eed6d70e97b6d12ef11e7e82ecca18166
yoraji <rajiy8299@gmail.com>
2024-12-30 05:15:32 +0100

    print_bits

--- print_bits/print_bits.c
+++ print_bits/print_bits.c
@@ -1,37 +1,14 @@
 #include <unistd.h>
 
-int calculate_the_lenght(int num)
-{
-	int counter = 0;
-	while(num)
-	{
-		num /= 2;
-		counter++;
-	}
-	return counter;
-}
-
 void	print_bits(unsigned char octet)
 {
-	int len = calculate_the_lenght(octet - '0');
-	int i=0;
-       	while(i < 8 - len)
+	int i = 8;
+	unsigned char bit;
+	while(i--)
 	{
-		write(1,"0",1);
-		i++;
+		bit = (octet >> i & 1) + '0';
+		write(1,&bit,1);
 	}
-	char arr[len];
-	i=len;
-	while(len--)
-	{
-		arr[len] = (octet % 2) + '0';
-		octet /= 2;
-	}
-	while(len < i)
-	{
-		write(1,&arr[len],1);
-		len++;
-	}	
 }
 
 #include <stdio.h>
